3394804794d66ed95e63a9e2b0181ae22ed851cc
Max Gfeller <mg@cyon.ch>
2014-11-25 11:52:44 +0100

    Check if md5 hash does not yet exist

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 var fs = require('fs')
 var crypto = require('crypto')
+var through = require('through')
 
 module.exports = function(giffer) {
-	giffer.pre('saveMetaData', function(next, id, url, metadata) {
-	  var hash = crypto.createHash('md5')
+	giffer.pre('saveMetaData', function(next, url, id, metadata) {
+		var hash = crypto.createHash('md5')
 	  var st = fs.createReadStream(giffer.outDir + '/' + id + '.gif')
-		st.on('data', function(data) {
-		  hash.update(data)
-		})
-		st.on('end', function() {
-		  metadata.md5 = hash.digest('hex')
-			next()
-		})
+		st.pipe(through(function data(data) {
+			hash.update(data)
+		}, function end() {
+			metadata.md5 = hash.digest('hex')
+			var alreadyDownloaded = false
+
+			giffer.urlDb.createReadStream().pipe(through(function data(data) {
+				if(data.value.meta.md5 === metadata.md5) alreadyDownloaded = true
+			}, function end() {
+				if(!alreadyDownloaded) next(id, url, metadata)
+			}))
+		}))
 	})
 }
